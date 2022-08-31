d8a9ffad8bb0a93b3a2919666ee328d4142d1e25
chavda-bhavik <bhavikvchavda@gmail.com>
2022-08-31 17:09:31 +0530

    [fix] Added pagination to notifications and Updated upload routes

--- apps/api/src/models/notification.ts
+++ apps/api/src/models/notification.ts
@@ -1,4 +1,6 @@
 import { Schema, model } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
 const tempSchema = new Schema({}, { strict: false });
+tempSchema.plugin(mongoosePaginate);
 const TempModel = model('notifications', tempSchema);
 export default TempModel;
